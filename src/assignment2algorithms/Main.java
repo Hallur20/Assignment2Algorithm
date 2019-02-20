d80c9a0c99ec2a266ffbf10e1c66caf11ce1f541
Hallur20 <hvn15@hotmail.com>
2019-02-20 12:29:57 +0100

    added project

---
+++ src/assignment2algorithms/Main.java
@@ -0,0 +1,29 @@
+package assignment2algorithms;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    private static ArrayList<String> textFileList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        FileReaderLogic l = new FileReaderLogic();
+        l.readFile(textFileList);
+        for (int i = 0; i < textFileList.size(); i++) {
+            System.out.println(textFileList.get(i));
+        }
+    }
+    
+    /*
+    linked symbol table is... scans the hashmap if the key is already in use, if it is then replace with new value,
+    do not continue scanning if key has been found.
+    
+    array symbol table is... (and what is flexible array) scan hashmap alphabeticaly where to put the key,
+    if same key but diffirent value just replace value (key is still at same index).
+    
+    hashed symbol table is... hash value is the position in the hash table, while the keys should be unique in the hash position.
+    
+    balanced tree symbol table is...
+    
+    
+    */
+}
