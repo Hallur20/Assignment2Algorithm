6119db99725449514c6d10ebb9b421b4296350d1
Hallur20 <hvn15@hotmail.com>
2019-02-20 21:41:59 +0100

    works, but i might redo some

--- src/assignment2algorithms/HashedSymbolTable.java
+++ src/assignment2algorithms/HashedSymbolTable.java
@@ -1,14 +1,192 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package assignment2algorithms;
 
-/**
- *
- * @author Hallur
- */
-public class HashedSymbolTable {
-    
+import assignment2algorithms.SequentialSearchST;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class HashedSymbolTable<Key, Value> {
+
+    private static final int INIT_CAPACITY = 4;
+
+    private int n;                                // number of key-value pairs
+    private int m;                                // hash table size
+    private SequentialSearchST<Key, Value>[] st;  // array of linked-list symbol tables
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public HashedSymbolTable() {
+        this(INIT_CAPACITY);
+    }
+
+    /**
+     * Initializes an empty symbol table with {@code m} chains.
+     *
+     * @param m the initial number of chains
+     */
+    public HashedSymbolTable(int m) {
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++) {
+            st[i] = new SequentialSearchST<Key, Value>();
+        }
+    }
+
+    // resize the hash table to have the given number of chains,
+    // rehashing all of the keys
+    private void resize(int chains) {
+        HashedSymbolTable<Key, Value> temp = new HashedSymbolTable<Key, Value>(chains);
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.m = temp.m;
+        this.n = temp.n;
+        this.st = temp.st;
+    }
+
+    // hash value between 0 and m-1
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty; {@code false}
+     * otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns true if this symbol table contains the specified key.
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key};
+     * {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the specified key in this symbol table.
+     *
+     * @param key the key
+     * @return the value associated with {@code key} in the symbol table;
+     * {@code null} if no such value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        int i = hash(key);
+        return st[i].get(key);
+    }
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting
+     * the old value with the new value if the symbol table already contains the
+     * specified key. Deletes the specified key (and its associated value) from
+     * this symbol table if the specified value is {@code null}.
+     *
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // double table size if average length of list >= 10
+        if (n >= 10 * m) {
+            resize(2 * m);
+        }
+
+        int i = hash(key);
+        if (!st[i].contains(key)) {
+            n++;
+        }
+        st[i].put(key, val);
+    }
+
+    /**
+     * Removes the specified key and its associated value from this symbol table
+     * (if the key is in this symbol table).
+     *
+     * @param key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+
+        int i = hash(key);
+        if (st[i].contains(key)) {
+            n--;
+        }
+        st[i].delete(key);
+
+        // halve table size if average length of list <= 2
+        if (m > INIT_CAPACITY && n <= 2 * m) {
+            resize(m / 2);
+        }
+    }
+
+    // return keys in symbol table as an Iterable
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                queue.add(key);
+            }
+        }
+        return queue;
+    }
+
+    public HashedSymbolTable(int n, int m, SequentialSearchST<Key, Value>[] st) {
+        this.n = n;
+        this.m = m;
+        this.st = st;
+    }
+
+    public void printHashMap(HashedSymbolTable<String, Integer> hm) {
+        String startAt = "{";
+        for (String name : hm.keys()) {
+
+            String key = name;
+            String value = hm.get(name).toString();
+            startAt+="(" + key + " -> " + value + ")\n";
+        }
+        System.out.println(startAt);
+    }
+
+    /**
+     * Unit tests the {@code SeparateChainingHashST} data type.
+     *
+     * @param args the command-line arguments
+     */
 }
--- src/assignment2algorithms/Main.java
+++ src/assignment2algorithms/Main.java
@@ -2,17 +2,45 @@ package assignment2algorithms;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
+
     private static ArrayList<String> textFileList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         FileReaderLogic l = new FileReaderLogic();
         l.readFile(textFileList);
-        for (int i = 0; i < textFileList.size(); i++) {
+        /* for (int i = 0; i < textFileList.size(); i++) {
             System.out.println(textFileList.get(i));
+        }*/
+
+        ArrayList<String> hardcodedWords = new ArrayList<>();
+        hardcodedWords.add("a");
+        hardcodedWords.add("x");
+        hardcodedWords.add("x");
+        hardcodedWords.add("x");
+        hardcodedWords.add("b");
+        hardcodedWords.add("m");
+        hardcodedWords.add("b");
+        
+        String toBe = "to be or not to be, that is the question";
+        toBe = toBe.replace(",", "");
+        String[] splitted = toBe.split(" ");
+        System.out.println(splitted.length);                
+
+        HashedSymbolTable<String, Integer> h = new HashedSymbolTable<>(splitted.length);
+        for (int i = 0; i < splitted.length; i++) {
+            String key = splitted[i];
+            if(h.contains(key)){
+                h.put(key, h.get(key)+1);
+                continue;
+            }
+            h.put(key, 1);
         }
-    }
-    
+        h.printHashMap(h);
+
+
     /*
     linked symbol table is... scans the hashmap if the key is already in use, if it is then replace with new value,
     do not continue scanning if key has been found.
@@ -25,5 +53,5 @@ public class Main {
     balanced tree symbol table is...
     
     
-    */
-}
+     */
+}}
