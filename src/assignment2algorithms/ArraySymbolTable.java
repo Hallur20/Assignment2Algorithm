769e910bfbe9492fc2cca25ac287fea9c82b9c6b
unknown <hvn15@hotmail.com>
2019-02-25 15:03:47 +0100

    added new src

--- src/assignment2algorithms/ArraySymbolTable.java
+++ src/assignment2algorithms/ArraySymbolTable.java
@@ -5,10 +5,108 @@
  */
 package assignment2algorithms;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Hallur
  */
-public class ArraySymbolTable {
-    
+import sun.awt.SunHints;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ArraySymbolTable<Key>  {
+    private static final int INIT_SIZE = 8;
+
+    private Key[] keys;     // symbol table keys
+    private Integer[] vals;   // symbol table values
+    private int n = 0;      // number of elements in symbol table
+
+    public ArraySymbolTable() {
+        keys = (Key[]) new Object[INIT_SIZE];
+        vals = (Integer[]) new Integer[INIT_SIZE];
+
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public String getAll() {
+        for (int i = 0; i < keys.length; i++)
+            System.out.println(keys[i].toString() + ":" + vals[i]);
+
+        return "Done";
+    }
+
+
+    private void resize(int capacity) {
+        Key[] tempk = (Key[]) new Object[capacity];
+        Integer[] tempv = (Integer[]) new Integer[capacity];
+        for (int i = 0; i < n; i++)
+            tempk[i] = keys[i];
+        for (int i = 0; i < n; i++)
+            tempv[i] = vals[i];
+        keys = tempk;
+        vals = tempv;
+    }
+
+    public void remove(Key key) {
+        for (int i = 0; i < n; i++) {
+            if (key.equals(keys[i])) {
+                keys[i] = keys[n-1];
+                vals[i] = vals[n-1];
+                keys[n-1] = null;
+                vals[n-1] = null;
+                n--;
+                return;
+            }
+        }
+    }
+
+    public int rank(String key)
+    {
+        int lo = 0, hi = n-1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(String.valueOf(keys[mid]));
+            if (cmp < 0) hi = mid - 1;
+            else if (cmp > 0) lo = mid + 1;
+            else return mid;
+        }
+        return lo;
+    }
+
+    public void put(Key key)  {
+        int val = 1;
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+
+        if (val == 0) {
+            remove(key);
+            return;
+        }
+
+        int i = rank(String.valueOf(key));
+
+        // key is already in table
+        if (i < n && String.valueOf(keys[i]).compareTo(String.valueOf(key)) == 0) {
+            vals[i] = vals[i]+val;
+            return;
+        }
+
+        // insert new key-value pair
+        if (n == keys.length) resize(2*keys.length);
+
+        for (int j = n; j > i; j--)  {
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+
+    }
+
 }
--- src/assignment2algorithms/Main.java
+++ src/assignment2algorithms/Main.java
@@ -2,28 +2,111 @@ package assignment2algorithms;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.time.StopWatch;
 
 public class Main {
+
     private static ArrayList<String> textFileList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         FileReaderLogic l = new FileReaderLogic();
         l.readFile(textFileList);
-        for (int i = 0; i < textFileList.size(); i++) {
-            System.out.println(textFileList.get(i));
-        }
+
+        HashedSymbolTable<String, Integer> hashedSymbolTable = new HashedSymbolTable<>();
+        SequentialSearchST<String, Integer> sequentialSearchST = new SequentialSearchST<>();
+        ArraySymbolTable<String> arraySymbolTable = new ArraySymbolTable();
+        BalancedTreeTable<String, Integer> balancedTreeTable = new BalancedTreeTable<>();
+
+        //---------------------------------------------------------------------//
+               /*Hashed Symbol Table*/
+        //timeHashedSymbolTable(hashedSymbolTable);
+        
+               /*Linked Symbol Table*/
+        //timeLinkedSymbolTable(sequentialSearchST);
+        
+               /*Array Symbol table*/
+        //timeArraySymbolTable(arraySymbolTable);
+        
+               /*Balanced Tree Symnbol Table*/
+        //timeBalancedTreeSymbolTable(balancedTreeTable);
     }
-    
-    /*
-    linked symbol table is... scans the hashmap if the key is already in use, if it is then replace with new value,
-    do not continue scanning if key has been found.
-    
-    array symbol table is... (and what is flexible array) scan hashmap alphabeticaly where to put the key,
-    if same key but diffirent value just replace value (key is still at same index).
-    
-    hashed symbol table is... hash value is the position in the hash table, while the keys should be unique in the hash position.
-    
-    balanced tree symbol table is...
-    
-    
-    */
+
+    public static void timeHashedSymbolTable(HashedSymbolTable<String, Integer> m) {
+        StopWatch s = new StopWatch();
+        s.start();
+        for (int i = 0; i < textFileList.size(); i++) {
+            String key = textFileList.get(i);
+            if (m.contains(key)) {
+                m.put(key, m.get(key) + 1);
+                continue;
+            }
+            m.put(key, 1);
+        }
+        s.stop();
+        m.printHashMap(m);
+        System.out.println("time in minutes: " + s.getTime(TimeUnit.MINUTES));
+        System.out.println("time in seconds: " + s.getTime(TimeUnit.SECONDS));
+        System.out.println("time in milli seconds: " + s.getTime(TimeUnit.MILLISECONDS));
+        System.out.println("time in micro seconds: " + s.getTime(TimeUnit.MICROSECONDS));
+    }
+
+    public static void timeLinkedSymbolTable(SequentialSearchST<String, Integer> m) {
+        StopWatch s = new StopWatch();
+        s.start();
+        for (int i = 0; i < textFileList.size(); i++) {
+            String key = textFileList.get(i);
+            if (m.contains(key)) {
+                m.put(key, m.get(key) + 1);
+                continue;
+            }
+            m.put(key, 1);
+        }
+        s.stop();
+        for (String st : m.keys()) {
+            System.out.println(st + " " + m.get(st));
+        }
+        System.out.println("time in minutes: " + s.getTime(TimeUnit.MINUTES));
+        System.out.println("time in seconds: " + s.getTime(TimeUnit.SECONDS));
+        System.out.println("time in milli seconds: " + s.getTime(TimeUnit.MILLISECONDS));
+        System.out.println("time in micro seconds: " + s.getTime(TimeUnit.MICROSECONDS));
+    }
+
+    public static void timeArraySymbolTable(ArraySymbolTable m) {
+        StopWatch s = new StopWatch();
+        s.start();
+        for (int i = 0; i < textFileList.size(); i++) {
+            String key = textFileList.get(i);
+            m.put(key);
+        }
+        s.stop();
+        try {
+            m.getAll();
+        }
+        catch(Exception e)
+        {}
+        System.out.println("time in minutes: " + s.getTime(TimeUnit.MINUTES));
+        System.out.println("time in seconds: " + s.getTime(TimeUnit.SECONDS));
+        System.out.println("time in milli seconds: " + s.getTime(TimeUnit.MILLISECONDS));
+        System.out.println("time in micro seconds: " + s.getTime(TimeUnit.MICROSECONDS));
+    }
+
+    public static void timeBalancedTreeSymbolTable(BalancedTreeTable<String, Integer> m) {
+        StopWatch s = new StopWatch();
+        s.start();
+        for (int i = 0; i < textFileList.size(); i++) {
+            String key = textFileList.get(i);
+            if (m.contains(key)) {
+                m.put(key, m.get(key) + 1);
+                continue;
+            }
+            m.put(key, 1);
+        }
+        s.stop();
+        m.printTree(m);
+        System.out.println("time in minutes: " + s.getTime(TimeUnit.MINUTES));
+        System.out.println("time in seconds: " + s.getTime(TimeUnit.SECONDS));
+        System.out.println("time in milli seconds: " + s.getTime(TimeUnit.MILLISECONDS));
+        System.out.println("time in micro seconds: " + s.getTime(TimeUnit.MICROSECONDS));
+    }
 }
