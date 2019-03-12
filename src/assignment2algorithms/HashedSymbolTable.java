2bc0468f6da9a3a2f9298bb15174d4c76c9d1629
Mokayed <marik.3000@hotmail.com>
2019-03-12 09:48:57 +0100

    rm

--- src/assignment2algorithms/HashedSymbolTable.java
+++ src/assignment2algorithms/HashedSymbolTable.java
@@ -11,18 +11,11 @@ public class HashedSymbolTable<Key, Value> {
     private int m;                                // hash table size
     private SequentialSearchST<Key, Value>[] st;  // array of linked-list symbol tables
 
-    /**
-     * Initializes an empty symbol table.
-     */
+ 
     public HashedSymbolTable() {
         this(INIT_CAPACITY);
     }
 
-    /**
-     * Initializes an empty symbol table with {@code m} chains.
-     *
-     * @param m the initial number of chains
-     */
     public HashedSymbolTable(int m) {
         this.m = m;
         st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
@@ -31,8 +24,7 @@ public class HashedSymbolTable<Key, Value> {
         }
     }
 
-    // resize the hash table to have the given number of chains,
-    // rehashing all of the keys
+
     private void resize(int chains) {
         HashedSymbolTable<Key, Value> temp = new HashedSymbolTable<Key, Value>(chains);
         for (int i = 0; i < m; i++) {
@@ -45,38 +37,21 @@ public class HashedSymbolTable<Key, Value> {
         this.st = temp.st;
     }
 
-    // hash value between 0 and m-1
     private int hash(Key key) {
         return (key.hashCode() & 0x7fffffff) % m;
     }
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     *
-     * @return the number of key-value pairs in this symbol table
-     */
+
     public int size() {
         return n;
     }
 
-    /**
-     * Returns true if this symbol table is empty.
-     *
-     * @return {@code true} if this symbol table is empty; {@code false}
-     * otherwise
-     */
+
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    /**
-     * Returns true if this symbol table contains the specified key.
-     *
-     * @param key the key
-     * @return {@code true} if this symbol table contains {@code key};
-     * {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+
     public boolean contains(Key key) {
         if (key == null) {
             throw new IllegalArgumentException("argument to contains() is null");
@@ -84,14 +59,7 @@ public class HashedSymbolTable<Key, Value> {
         return get(key) != null;
     }
 
-    /**
-     * Returns the value associated with the specified key in this symbol table.
-     *
-     * @param key the key
-     * @return the value associated with {@code key} in the symbol table;
-     * {@code null} if no such value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+
     public Value get(Key key) {
         if (key == null) {
             throw new IllegalArgumentException("argument to get() is null");
@@ -101,16 +69,7 @@ public class HashedSymbolTable<Key, Value> {
         return st[i].get(key);
     }
 
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting
-     * the old value with the new value if the symbol table already contains the
-     * specified key. Deletes the specified key (and its associated value) from
-     * this symbol table if the specified value is {@code null}.
-     *
-     * @param key the key
-     * @param val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+ 
     public void put(Key key, Value val) {
         if (key == null) {
             throw new IllegalArgumentException("first argument to put() is null");
@@ -120,7 +79,6 @@ public class HashedSymbolTable<Key, Value> {
             return;
         }
 
-        // double table size if average length of list >= 10
         if (n >= 10 * m) {
             resize(2 * m);
         }
@@ -132,13 +90,7 @@ public class HashedSymbolTable<Key, Value> {
         st[i].put(key, val);
     }
 
-    /**
-     * Removes the specified key and its associated value from this symbol table
-     * (if the key is in this symbol table).
-     *
-     * @param key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+  
     public void delete(Key key) {
         if (key == null) {
             throw new IllegalArgumentException("argument to delete() is null");
@@ -150,13 +102,12 @@ public class HashedSymbolTable<Key, Value> {
         }
         st[i].delete(key);
 
-        // halve table size if average length of list <= 2
+        
         if (m > INIT_CAPACITY && n <= 2 * m) {
             resize(m / 2);
         }
     }
 
-    // return keys in symbol table as an Iterable
     public Iterable<Key> keys() {
         Queue<Key> queue = new LinkedList<>();
         for (int i = 0; i < m; i++) {
@@ -184,9 +135,4 @@ public class HashedSymbolTable<Key, Value> {
         System.out.println(startAt);
     }
 
-    /**
-     * Unit tests the {@code SeparateChainingHashST} data type.
-     *
-     * @param args the command-line arguments
-     */
 }
